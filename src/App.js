315b844ca4df9606900aaf3a69d4aab43d833398
navvvvvv <111989295+navvvvvv@users.noreply.github.com>
2023-01-03 16:43:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Resturant from './component/Basics/Resturant';
+  import UseState from './component/Hooks/useState';
+import UseEffect from './component/Hooks/useEffect';
+import UseReducer from './component/Hooks/useReducer';
+import Todo from './component/todoreact/todo';
+import Temp from './component/weather/temp';
+
+const App = () => {
+  return  <>
+    <UseEffect />
+    
+  </>
+  
+};
+
+export default App
